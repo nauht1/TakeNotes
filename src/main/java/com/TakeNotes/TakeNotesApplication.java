6b4158f1a7135910aa71c94402ff5541c0cd85ed
MinhThuan <thuanngo454@gmail.com>
2024-06-01 00:36:17 +0700

    init

---
+++ src/main/java/com/TakeNotes/TakeNotesApplication.java
@@ -0,0 +1,13 @@
+package com.TakeNotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakeNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TakeNotesApplication.class, args);
+	}
+
+}
